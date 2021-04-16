ef589c5c8018d5bfa1f61ddd79157d08f50fa84e
julesdata <74705142+julesdata@users.noreply.github.com>
2021-04-16 12:36:17 +0900

    Update beer_sales.sql

--- 프로시져/beer_sales.sql
+++ 프로시져/beer_sales.sql
@@ -75,3 +75,66 @@ AND e.mng_area1 > ''
 AND e.mng_area2 > ''
 AND e.sales_grd > ''
 GROUP BY b.sales_wk,e.mng_area1,e.mng_area2, e.sales_grd;
+
+
+#join
+
+SELECT *
+FROM (#한맥
+	SELECT b.sales_wk, e.mng_area1,e.mng_area2, e.sales_grd, d.barcode_no, d.goods_nm, COUNT(DISTINCT a.drim_store_id) store_cnt, sum(a.sales_amt) sales_amt
+	FROM temp_pos_dd_sales a, tb_dt_dic b, tb_pos_barcode_mst d
+	, (SELECT * from dw_store_mst 
+	   WHERE drim_store_id IN(SELECT DISTINCT drim_store_id from dw_store_mm_sum  
+		                       WHERE sales_mm BETWEEN 202102 AND 202103 
+									  AND day_cnt > 20 
+									  GROUP BY drim_store_id HAVING COUNT(DISTINCT sales_mm)=2)
+		) e
+	WHERE 1=1
+	AND a.sales_date = b.sales_date
+	# AND b.SALES_wk=202107
+	AND b.sales_mm BETWEEN 202102 AND 202103
+	AND a.barcode_no = d.barcode_no
+	# 한맥 바코드
+	AND d. barcode_no IN('8801021105598','8801021105604','8801021105833','8801021105833')
+	AND d.valid_yn = 'Y'
+	AND d.maker_nm > ''
+	AND a.drim_store_id = e.drim_store_id
+	AND e.store_type IN ('슈퍼마켓', '편의점')
+	AND e.pos_co_cd NOT IN ('POS1607')
+	AND e.mng_area1 > ''
+	AND e.mng_area2 > ''
+	AND e.sales_grd > ''
+	GROUP BY b.sales_wk,e.mng_area1,e.mng_area2, e.sales_grd, d.barcode_no, d.goods_nm
+	) x1
+JOIN 
+	# 맥주
+	(SELECT b.sales_wk, e.mng_area1,e.mng_area2, e.sales_grd,
+				COUNT(DISTINCT a.drim_store_id) all_store_cnt,
+				sum(case when d.category_nm4 IN ('맥주') then a.sales_amt end) beer_amt_local,
+				sum(case when d.category_nm4 IN ('수입맥주') then a.sales_amt end) beer_amt_import
+	FROM temp_pos_dd_sales a, tb_dt_dic b, tb_pos_barcode_mst d
+	, (SELECT * from dw_store_mst 
+	   WHERE drim_store_id IN(SELECT DISTINCT drim_store_id from dw_store_mm_sum  
+		                       WHERE sales_mm BETWEEN 202102 AND 202103 
+									  AND day_cnt > 20 
+									  GROUP BY drim_store_id HAVING COUNT(DISTINCT sales_mm)=2)
+		) e
+	WHERE 1=1
+	AND a.sales_date = b.sales_date
+	#AND b.SALES_wk=202107
+	AND b.sales_mm BETWEEN 202102 AND 202103
+	AND a.barcode_no = d.barcode_no
+	# 맥주 카테고리
+	AND d.category_nm4 IN('맥주', '수입맥주')
+	AND d.valid_yn = 'Y'
+	AND d.maker_nm > ''
+	AND a.drim_store_id = e.drim_store_id
+	AND e.store_type IN ('슈퍼마켓', '편의점')
+	AND e.pos_co_cd NOT IN ('POS1607')
+	AND e.mng_area1 > ''
+	AND e.mng_area2 > ''
+	AND e.sales_grd > ''
+	GROUP BY b.sales_wk,e.mng_area1,e.mng_area2, e.sales_grd
+	) x2
+USING (sales_wk, mng_area1, mng_area2, sales_grd)
+
